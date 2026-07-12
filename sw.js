7e2a3fb7af379c82cf0a8f7235dcfc5fd3f23182
h-alkulaib <h.alkulaib05@gmail.com>
2026-07-12 13:58:06 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "buddys-money-v1";
+const CACHE_NAME = "Husain's-money-v1";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
